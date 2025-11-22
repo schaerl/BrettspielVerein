2d69afe865fdf387a9f5cbec5921c79acf6b3615
Lukas Schär <luki.schaer@hispeed.ch>
2025-11-22 11:27:50 +0100

    034: Remove vuetify config and add NuxtUi config

--- vue/nuxt.config.ts
+++ vue/nuxt.config.ts
@@ -1,65 +1,3 @@
-import { type ThemeDefinition } from "vuetify";
-// https://nuxt.com/docs/api/configuration/nuxt-config
-const myCustomLightTheme: ThemeDefinition = {
-  dark: false,
-  colors: {
-    primary: "#F2CC8F",
-    surfaceTint: "#745B0C",
-    onPrimary: "#FFFFFF",
-    primaryContainer: "#FFDF91",
-    onPrimaryContainer: "#594400",
-    secondary: "#F4F1DE",
-    onSecondary: "#FFFFFF",
-    secondaryContainer: "#F2E1BB",
-    onSecondaryContainer: "#51462A",
-    tertiary: "#486649",
-    onTertiary: "#FFFFFF",
-    tertiaryContainer: "#C9ECC8",
-    onTertiaryContainer: "#304D33",
-    error: "#E56666",
-    onError: "#FFFFFF",
-    errorContainer: "#FFDAD6",
-    onErrorContainer: "#93000A",
-    background: "#FFF8F1",
-    onBackground: "#1F1B13",
-    surface: "#FFF8F1",
-    onSurface: "#1F1B13",
-    surfaceVariant: "#ECE1CF",
-    onSurfaceVariant: "#4C4639",
-    outline: "#7E7667",
-    outlineVariant: "#CFC5B4",
-    shadow: "#000000",
-    scrim: "#000000",
-    inverseSurface: "#343027",
-    inverseOnSurface: "#F9F0E2",
-    inversePrimary: "#E4C36C",
-    primaryFixed: "#FFDF91",
-    onPrimaryFixed: "#241A00",
-    primaryFixedDim: "#E4C36C",
-    onPrimaryFixedVariant: "#594400",
-    secondaryFixed: "#F2E1BB",
-    onSecondaryFixed: "#231B04",
-    secondaryFixedDim: "#D6C5A0",
-    onSecondaryFixedVariant: "#51462A",
-    tertiaryFixed: "#C9ECC8",
-    onTertiaryFixed: "#04210B",
-    tertiaryFixedDim: "#AECFAD",
-    onTertiaryFixedVariant: "#304D33",
-    surfaceDim: "#E1D9CC",
-    surfaceBright: "#FFF8F1",
-    surfaceContainerLowest: "#FFFFFF",
-    surfaceContainerLow: "#FCF2E5",
-    surfaceContainer: "#F6EDDF",
-    surfaceContainerHigh: "#F0E7D9",
-    surfaceContainerHighest: "#EAE1D4",
-    header: "#3D405B",
-    footer: "#81B29A",
-  },
-  variables: {
-    "activated-opacity": 0,
-  },
-};
-
 export default defineNuxtConfig({
   app: {
     head: {
@@ -101,7 +39,7 @@ export default defineNuxtConfig({
     },
   },
   compatibilityDate: "2024-11-01",
-  css: ["./app/assets/css/global.scss"],
+  css: ["~/assets/css/global.scss"],
   devServer: {
     port: 8000,
   },
@@ -113,21 +51,6 @@ export default defineNuxtConfig({
   },
   features: {
     inlineStyles: false,
-  },
-  webpack: {
-    extractCSS: true,
-    optimization: {
-      splitChunks: {
-        cacheGroups: {
-          styles: {
-            name: "styles",
-            test: /\.(css|vue)$/,
-            chunks: "all",
-            enforce: true,
-          },
-        },
-      },
-    },
   },
   fonts: {
     defaults: {
@@ -148,7 +71,7 @@ export default defineNuxtConfig({
   imports: {
     dirs: ["types/*.ts"],
   },
-  modules: ["vuetify-nuxt-module", "@nuxt/fonts", "@vueuse/nuxt"],
+  modules: ["@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt"],
   nitro: {
     prerender: {
       ignore: ["/200.html", "/404.html"],
@@ -159,30 +82,9 @@ export default defineNuxtConfig({
       apiUrl: "",
     },
   },
-  vuetify: {
-    moduleOptions: {
-      disableVuetifyStyles: true,
-      styles: {
-        configFile: "assets/css/components.scss",
-      },
-    },
-    vuetifyOptions: {
-      defaults: {
-        global: {
-          ripple: false,
-        },
-      },
-      theme: {
-        defaultTheme: "myCustomLightTheme",
-        themes: {
-          myCustomLightTheme,
-        },
-        variations: {
-          colors: ["primary", "secondary"],
-          lighten: 0,
-          darken: 1,
-        },
-      },
-    },
-  },
+  vite: {
+    build: {
+      cssCodeSplit: false
+    }
+  }
 });
