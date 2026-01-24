67f5e9cf9a248e126043c927c971b810c4edd207
Lukas Schär <luki.schaer@hispeed.ch>
2026-01-24 18:52:25 +0100

    030: Add basic events page
    It doesn't have much design, and it's not linked to from anywhere yet, but it
    works, and has pagination

---
+++ vue/app/pages/events.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+const { repository: eventRepository } = useEventRepository();
+const itemsPerPage = 10;
+const page = ref(1);
+const refreshing = ref(false);
+
+const { data } = useLazyAsyncData(() => eventRepository.getPagedEventData(page.value, itemsPerPage), { server: false });
+
+const events = computed(() => data.value?.data ?? []);
+const totalEvents = computed(() => data.value?.total ?? 0);
+
+async function updatePage(newPage: number) {
+  refreshing.value = true;
+  page.value = newPage;
+  await refreshNuxtData();
+  refreshing.value = false;
+}
+</script>
+
+<template>
+  <UContainer>
+    <Section title="Events">
+      <BVZSheet
+        class="bg-secondary rounded-xl flex flex-col gap-4"
+      >
+        <EventCard
+          v-for="eventData in events"
+          :key="eventData.id"
+          :data="eventData"
+          class="bg-white rounded-xl"
+        />
+        <UPagination
+          class="self-center"
+          :disabled="refreshing"
+          :items-per-page
+          :page="page"
+          :total="totalEvents"
+          @update:page="updatePage"
+        />
+      </BVZSheet>
+    </Section>
+  </UContainer>
+</template>
+
+<style scoped>
+</style>
